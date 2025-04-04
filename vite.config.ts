bfae97ca549130ff21f24d8d293d545990431f15
Aishwarya Manek <amnk.459@gmail.com>
2025-04-04 20:14:29 +0200

    Fix2

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './', // Add this for GitHub Pages deployment
+  base: '', // Add this for GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
